6a42e7f11a477810126cfb056a4d84afa5b3533b
Eric Johnson & Sterling Houghton <student@epicodus.com>
2014-08-06 11:32:48 -0700

    edits & lists doctor's accepted insurance

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -2,17 +2,52 @@ require 'pg'
 
 class Doctor
 
-  attr_reader :name, :specialty, :id
+  attr_reader :name, :specialty, :id, :insurance
 
   def initialize attributes
     @name = attributes[:name]
     @specialty = attributes[:specialty]
     @id = attributes[:id]
+    @insurance = attributes[:insurance]
   end
 
   def self.all
     doctors = []
     results = DB.exec("SELECT * FROM doctors;")
+    results.each do |result|
+      attributes = {
+        :name => result['name'],
+        :specialty => result['specialty'].to_i,
+        :insurance => result['insurance'].to_i,
+        :id => result['id'].to_i
+      }
+      doctors << Doctor.new(attributes)
+    end
+    doctors
+  end
+
+  def save
+    results = DB.exec("INSERT INTO doctors (name) VALUES ('#{name}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def edit_specialty number
+    DB.exec("UPDATE doctors SET specialty = #{number} WHERE id = #{id};")
+    @specialty = number.to_i
+  end
+
+  def edit_insurance number
+    DB.exec("UPDATE doctors SET insurance = #{number} WHERE id = #{id};")
+    @insurance = number.to_i
+  end
+
+  def == another_doctor
+    self.name == another_doctor.name && self.id == another_doctor.id
+  end
+
+  def self.list_by_specialty number
+    doctors = []
+    results = DB.exec("SELECT * FROM doctors WHERE specialty = #{number};")
     results.each do |result|
       attributes = {
         :name => result['name'],
@@ -24,29 +59,14 @@ class Doctor
     doctors
   end
 
-  def save
-    results = DB.exec("INSERT INTO doctors (name, specialty) VALUES ('#{name}', '#{specialty}') RETURNING id;")
-    @id = results.first['id'].to_i
-  end
-
-  def edit_specialty number
-    DB.exec("UPDATE doctors SET specialty = #{number} WHERE id = #{id};")
-    @specialty = number.to_i
-  end
-
-  def == another_doctor
-    self.name == another_doctor.name && self.id == another_doctor.id
-  end
-
-  def self.list_by_specialty number
-    # doctors = []
-    # results = DB.exec(SELECT * docs WHERE speciality = #{number})
+  def self.list_by_insurance number
     doctors = []
-    results = DB.exec("SELECT * FROM doctors WHERE specialty = #{number};")
+    results = DB.exec("SELECT * FROM doctors WHERE insurance = #{number};")
     results.each do |result|
       attributes = {
         :name => result['name'],
         :specialty => result['specialty'].to_i,
+        :insurance => result['insurance'].to_i,
         :id => result['id'].to_i
       }
       doctors << Doctor.new(attributes)
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -31,13 +31,40 @@ describe 'Doctor' do
 
   describe '.list_by_specialty' do
     it 'lists all doctors with a given specialty' do
-      new_doctor1 = Doctor.new({:name=>'Joe', :specialty=>1})
+      new_doctor1 = Doctor.new({:name=>'Joe'})
       new_doctor1.save
-      new_doctor2 = Doctor.new({:name=>'Jim', :specialty=>2})
+      new_doctor1.edit_specialty(1)
+      new_doctor2 = Doctor.new({:name=>'Jim'})
       new_doctor2.save
-      new_doctor3 = Doctor.new({:name=>'Jet', :specialty=>2})
+      new_doctor2.edit_specialty(2)
+      new_doctor3 = Doctor.new({:name=>'Jet'})
       new_doctor3.save
+      new_doctor3.edit_specialty(2)
       expect(Doctor.list_by_specialty(2)).to eq [new_doctor2, new_doctor3]
+    end
+  end
+
+  describe 'edit_insurance' do
+    it 'changes doctor insurance' do
+      new_doctor = Doctor.new({:name=>'Joe', :specialty=>1})
+      new_doctor.save
+      new_doctor.edit_insurance(1)
+      expect(new_doctor.insurance).to eq 1
+    end
+  end
+
+  describe '.list_by_insurance' do
+    it 'lists all doctors that accept given insurance' do
+      new_doctor1 = Doctor.new({:name=>'Joe'})
+      new_doctor1.save
+      new_doctor1.edit_insurance(1)
+      new_doctor2 = Doctor.new({:name=>'Jim'})
+      new_doctor2.save
+      new_doctor2.edit_insurance(1)
+      new_doctor3 = Doctor.new({:name=>'Jet'})
+      new_doctor3.save
+      new_doctor3.edit_insurance(2)
+      expect(Doctor.list_by_insurance(1)).to eq [new_doctor1, new_doctor2]
     end
   end
 end
