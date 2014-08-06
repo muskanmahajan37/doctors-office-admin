aae6e75e2c327e730f495f0bf33d1efe9d9aed01
Eric Johnson & Sterling Houghton <student@epicodus.com>
2014-08-06 10:23:38 -0700

    returns all patient

--- lib/patient.rb
+++ lib/patient.rb
@@ -7,5 +7,22 @@ class Patient
   def initialize attributes
     @name = attributes[:name]
     @birthdate = attributes[:birthdate]
+    @id = attributes[:id]
+    @doctor_id = attributes[:doctor_id]
+  end
+
+  def self.all
+    patients = []
+    results = DB.exec("SELECT * FROM patients;")
+    results.each do |result|
+      attributes = {
+        :name => result['name'],
+        :birthdate => result['birthdate'],
+        :id => result['id'],
+        :doctor_id => result['doctor_id']
+      }
+      patients << Patient.new(attributes)
+    end
+    patients
   end
 end
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -1,8 +1,17 @@
 require 'doc_office_spec'
 
 describe 'patient' do
+
   it 'initializes with a hash' do
     new_patient = Patient.new({:name=>'Joe', :birthdate=>'1960/05/12'})
     expect(new_patient).to be_an_instance_of Patient
   end
+
+  describe '.all' do
+      it 'returns a blank array' do
+      expect(Patient.all).to eq []
+    end
+  end
+
 end
+
