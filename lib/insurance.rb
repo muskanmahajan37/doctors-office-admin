2914f57f26ea32b9ecb23ba522e2d124ea210eb1
Eric Johnson & Sterling Houghton <student@epicodus.com>
2014-08-06 12:01:48 -0700

    initialize insurace

--- lib/insurance.rb
+++ lib/insurance.rb
@@ -4,7 +4,20 @@ class Insurance
   attr_reader :company, :id
 
   def initialize attributes
-
+    @company = attributes[:company]
+    @id = attributes[:id]
   end
 
+  def self.all
+    insurance = []
+    results = DB.exec("SELECT * FROM insurance;")
+    results.each do |result|
+      attributes = {
+        :company => result['company'],
+        :id => result['id'].to_i
+      }
+      insurance << Insurance.new(attributes)
+    end
+    insurance
+  end
 end
--- spec/insurance_spec.rb
+++ spec/insurance_spec.rb
@@ -1,2 +1,8 @@
 require 'doc_office_spec'
 
+describe 'Insurance' do
+  it 'initializes with a hash' do
+    new_insurance = Insurance.new({:company=>'Red Shield'})
+    expect(new_insurance).to be_an_instance_of Insurance
+  end
+end
