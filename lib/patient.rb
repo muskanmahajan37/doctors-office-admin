f1ae44988f5ebb494a4986e6c2490801d6edeca3
Eric Johnson & Sterling Houghton <student@epicodus.com>
2014-08-06 10:32:34 -0700

    saves patients

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,7 +2,7 @@ require 'pg'
 
 class Patient
 
-  attr_reader :name, :birthdate
+  attr_reader :name, :birthdate, :id, :doctor_id
 
   def initialize attributes
     @name = attributes[:name]
@@ -18,11 +18,20 @@ class Patient
       attributes = {
         :name => result['name'],
         :birthdate => result['birthdate'],
-        :id => result['id'],
-        :doctor_id => result['doctor_id']
+        :id => result['id'].to_i,
+        :doctor_id => result['doctor_id'].to_i
       }
       patients << Patient.new(attributes)
     end
     patients
+  end
+
+  def save
+    results = DB.exec("INSERT INTO patients (name, birthdate) VALUES ('#{name}', '#{birthdate}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def == another_patient
+    self.name == another_patient.name && self.id == another_patient.id
   end
 end
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -3,13 +3,21 @@ require 'doc_office_spec'
 describe 'patient' do
 
   it 'initializes with a hash' do
-    new_patient = Patient.new({:name=>'Joe', :birthdate=>'1960/05/12'})
+    new_patient = Patient.new({:name=>'Joe', :birthdate=>'1960-05-12'})
     expect(new_patient).to be_an_instance_of Patient
   end
 
   describe '.all' do
-      it 'returns a blank array' do
+    it 'returns a blank array' do
       expect(Patient.all).to eq []
+    end
+  end
+
+  describe 'save' do
+    it 'saves a patient' do
+      new_patient = Patient.new({:name=>'Joe', :birthdate=>'1960-05-12'})
+      new_patient.save
+      expect(Patient.all[0]).to eq new_patient
     end
   end
 
